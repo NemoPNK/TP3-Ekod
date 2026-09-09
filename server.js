a81b0cd75bb8a06f9af00b20306c446b7e6ef431
Leopold David <leopolddavid@MacBook-Pro-de-tkt.local>
2026-09-09 09:04:19 +0200

    Very first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!');
+});
+
+app.listen(port, () => {
+    console.log(`serveur sur http://localhost:${port}`);
+})
+
+// Routes API
+
+app.post('/api/tasks', (req, res) => {
+    const newTask = req.body;
+    MyTasks.push(newTask);
+
+    console.log("Task : ", newTask)
+    res.status(201).json({
+        message: 'Post ok',
+        task: newTask
+    });
+});
+
+app.get('/api/tasks', (req, res) => {
+    console.log("Task : ", MyTasks)
+    res.status(200).json({
+        message: 'Get ok',
+        task: MyTasks
+    });
+});
+
+app.put('/api/tasks/:id', (req, res) => {
+    const taskID = req.params.id;
+    const putTask = req.body;
+
+    res.status(200).json({
+        message: 'Put ok',
+        task: putTask
+    });
+});
+
+app.delete('/api/tasks/:id', (req, res) => {
+    const taskID = req.params.id;
+    
+})
+
+// Tableau
+
+const MyTasks = [
+    { id: 0, complété: false, titre: "monter" },
+    { id: 1, complété: false, titre: "up" },
+    { id: 2, complété: false, titre: "down" },
+    { id: 3, complété: false, titre: "boom" },
+    { id: 4, complété: false, titre: "bam" }
+];
